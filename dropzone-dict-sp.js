55d1b429aa3354a35cf3d50d5226d31a2dc11db9
Luis Urbina <luis@luisurbina.net>
2021-01-28 09:35:42 -0400

    dropzone-dict-sp.js

---
+++ dropzone-dict-sp.js
@@ -0,0 +1,44 @@
+/**
+ * Dropzone Default Language Translate pack
+ *
+ * @lang Spanish
+ * @lang-ISO es-ES
+ *
+ * @author LuisUrbinaNet(luis@luisurbina.net) @luisurbinanet
+ * @license Public Domain
+ * @see http://github.com/luisurbinanet/dropzone-dict-sp
+ */
+
+Dropzone.prototype.defaultOptions.dictDefaultMessage
+//  = "Drop files here to upload";
+  = "Arrastra los archivos aquí para subirlos";
+Dropzone.prototype.defaultOptions.dictFallbackMessage
+//  = "Your browser does not support drag'n'drop file uploads.";
+  = "Su navegador no admite la carga de archivos mediante la función de arrastrar y soltar.";
+Dropzone.prototype.defaultOptions.dictFallbackText
+//  = "Please use the fallback form below to upload your files like in the olden days.";
+  = "Utilice el formulario de respaldo a continuación para cargar sus archivos como en los viejos tiempos.";
+Dropzone.prototype.defaultOptions.dictFileTooBig
+//  = "File is too big ({{filesize}}MB). Max filesize: {{maxFilesize}}MB.";
+  = "El archivo es demasiado grande ({{filesize}}MB). tamaño Max. de archivo: {{maxFilesize}}MB.";
+Dropzone.prototype.defaultOptions.dictInvalidFileType
+//  = "You can't upload files of this type.";
+  = "No puede cargar archivos de este tipo.";
+Dropzone.prototype.defaultOptions.dictResponseError
+//  = "Server responded with {{statusCode}} code.";
+  = "El servidor respondió con el código {{statusCode}}.";
+Dropzone.prototype.defaultOptions.dictCancelUpload
+//  = "Cancel upload";
+  = "Cancelar carga";
+Dropzone.prototype.defaultOptions.dictCancelUploadConfirmation
+//  = "Are you sure you want to cancel this upload?";
+  = "¿Estás seguro de que deseas cancelar esta carga?";
+Dropzone.prototype.defaultOptions.dictRemoveFile
+//  = "Remove file";
+  = "Remover archivo";
+Dropzone.prototype.defaultOptions.dictRemoveFileConfirmation
+//  = null;
+  = null;
+Dropzone.prototype.defaultOptions.dictMaxFilesExceeded
+//  = "You can only upload {{maxFiles}} files.";
+  = "Solo puede cargar archivos {{maxFiles}}.";
